03e26da9b2f8189745743f7837a39fe4cf709326
eichisanden <parkizz@gmail.com>
2018-12-15 21:59:47 +0000

    refactoring: Fix to prepend from init.rb directly

--- lib/markdown_task_list_plugin.rb
+++ lib/markdown_task_list_plugin.rb
@@ -1,20 +1,13 @@
 module MarkdownTaskListPlugin
-  module Patch
-    def self.included(base)
-      base.prepend(ToHtmlWithTaskList)
-    end
-
-    module ToHtmlWithTaskList
-      def to_html(*args)
-        html = super(*args)
-        html = html.gsub(/\[[xX]\]/) do
-          %(<input type="checkbox" checked disabled>)
-        end
-        html.gsub(/\[ \]/) do
-          %(<input type="checkbox" disabled>)
+  module ToHtmlWithTaskList
+    def to_html(*args)
+      html = super(*args)
+      html = html.gsub(/\[[xX]\]/) do
+        %(<input type="checkbox" checked disabled>)
       end
-    end
-
+      html.gsub(/\[ \]/) do
+        %(<input type="checkbox" disabled>)
+      end
     end
   end
 end
