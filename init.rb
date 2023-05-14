d3f65c6dccc64934b3b43b1f71c73f1c96df8402
eichisanden <eichisanden@users.noreply.github.com>
2023-05-14 22:26:24 +0900

    Unify Plugin settings in one place.
    It worked for both Redmine3 and Redmine4, so unify the initialization logic.

--- init.rb
+++ init.rb
@@ -10,16 +10,6 @@ Redmine::Plugin.register :redmine_markdown_task_list do
   requires_redmine :version_or_higher => '3.0.0'
 end
 
-Rails.configuration.to_prepare do
-  Redmine::WikiFormatting.format_names.each do |format_name|
-    formatter = Redmine::WikiFormatting.formatter_for(format_name)
-    if format_name == "markdown" then
-      formatter.prepend(MarkdownTaskListPlugin::ToHtmlWithTaskList)
-    end
-  end
-end
-
-# for Redmine 5
 Rails.application.config.after_initialize do
   Redmine::WikiFormatting.format_names.each do |format_name|
     formatter = Redmine::WikiFormatting.formatter_for(format_name)
