37e2a5d12c2493712a5bc14d58f1565bc0209a41
eichisanden <eichisanden@users.noreply.github.com>
2024-02-17 23:32:55 +0900

    Bumped version to 1.0.4

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_markdown_task_list do
   name 'Redmine Markdown Task List plugin'
   author 'eichisanden'
   description 'This plugin adds Task List to Markdown wiki formatting.'
-  version '1.0.3'
+  version '1.0.4'
   url 'https://github.com/eichisanden/redmine_markdown_task_list'
   author_url 'https://github.com/eichisanden'
   requires_redmine :version_or_higher => '3.0.0'
